5cc8a2240e462799f1e6c021513217b2e8e2bb17
Connor Sutton <connorsutton07@gmail.com>
2021-04-15 00:19:08 -0500

    fixed typos

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
 from train import run
 
 if __name__ == "__main__":
-    run(training_mode=True, pretrained=False, num_episodes=1000)
+    run(training_mode=True, pretrained=True, num_episodes=9000)
