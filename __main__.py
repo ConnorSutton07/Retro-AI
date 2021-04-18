e739d704863081478a70ad91268c1c5b9bb4628f
Connor Sutton <connorsutton07@gmail.com>
2021-04-18 03:05:55 -0500

    removed argparsing, replacing with ui options

--- __main__.py
+++ __main__.py
@@ -1,10 +1,5 @@
-from train import run
-import argparse
+from driver import Driver
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Retro AI")
-    parser.add_argument("--episodes", type=int, nargs='?',
-                        const=True, default=1000, 
-                        help="Number of training episodes")
-    args = parser.parse_args()
-    run(training_mode=False, pretrained=True, num_episodes=300)
+    driver = Driver()
+    driver.run()
