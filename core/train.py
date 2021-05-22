3d8326458b7b96383a0a06eeb1ca5605fbb436eb
Connor Sutton <connorsutton07@gmail.com>
2021-05-22 10:56:18 -0500

    allows for custom reward functions

--- core/train.py
+++ core/train.py
@@ -2,28 +2,31 @@ import gym_super_mario_bros
 from core.deepQ.agent import DQNAgent
 from core.environment import make_env
 from core.config import * 
+from core.reward import rewards
 from tqdm import tqdm
 import torch
 import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 import retro
-#import keyboard
+import cv2
+import keyboard
 
 def run(training_mode: bool, 
         pretrained: bool,
         num_episodes: int,
         save_path: str,
         load_path: str = None,
-        game: str = 'SuperMarioBros-1-1-v0' 
+        game: str = 'SuperMarioWorld-Snes' 
         ) -> None:
 
     print("Creating environment...")
     #env = gym_super_mario_bros.make(game)
-    env = retro.make('SuperMarioBros-Nes', state='Level1-1')
+    env = retro.make(game, state='YoshiIsland1')
     env = make_env(env)
     observation_space = env.observation_space.shape
-    action_space = env.action_space.n 
+    action_space = env.action_space.n
+    reward_function = rewards[game] 
 
     print("Initializing agent...")
     agent = DQNAgent(
@@ -44,9 +47,11 @@ def run(training_mode: bool,
 
     env.reset()
     total_rewards = []
-
+    force_quit = False
+    iterator = tqdm(range(num_episodes))
     print("Starting...")
-    for ep_num in tqdm(range(num_episodes)):
+    
+    for ep_num in iterator:
         state = env.reset()
         state = torch.Tensor([state])
         total_reward = 0
@@ -54,12 +59,15 @@ def run(training_mode: bool,
         counter = 0
         terminal = False
         prev_reward = 0
+        prev_info = None
         while not terminal:
-            #if not training_mode:
-            env.render()
+            if not training_mode:
+                env.render()
+
             action = agent.act(state)
             steps += 1
-            state_next, reward, terminal, info = env.step(int(action[0]))
+            state_next, _, _, info = env.step(int(action[0]))
+            reward, terminal = reward_function(info, prev_info)
             total_reward += reward
 
             counter = counter + 1 if prev_reward >= total_reward else 0
@@ -68,27 +76,36 @@ def run(training_mode: bool,
 
             state_next = torch.Tensor([state_next])
             reward = torch.tensor([reward]).unsqueeze(0)
-            terminal = torch.tensor([int(terminal)]).unsqueeze(0)
+            terminal_t = torch.tensor([int(terminal)]).unsqueeze(0)
 
             if training_mode:
-                agent.remember(state, action, reward, state_next, terminal)
+                agent.remember(state, action, reward, state_next, terminal_t)
                 agent.experience_replay()
 
             prev_reward = total_reward
             state = state_next
+            prev_info = info.copy()
+
+            if keyboard.is_pressed('esc'):
+                print("Exiting...")
+                force_quit = True
+                iterator.close()
+                break
+
             
         total_rewards.append(total_reward)
         print("Total reward after episode {} is {}".format(ep_num + 1, total_rewards[-1]))
         num_episodes += 1      
 
-    if training_mode:
-        print("Saving model...")
-        agent.save(total_rewards)
-    
-    env.close()  
+    if not training_mode:
+        env.render(close=True)
 
-    if num_episodes > 500:
-        plt.title("Episodes trained vs. Average Rewards (per 500 eps)")
-        plt.plot([0 for _ in range(500)] + 
-                 np.convolve(total_rewards, np.ones((500,))/500, mode="valid").tolist())
-        plt.show()
+    if not force_quit:  
+        if training_mode:
+            print("Saving model...")
+            agent.save(total_rewards)
+        if num_episodes > 500:
+            plt.title("Episodes trained vs. Average Rewards (per 500 eps)")
+            plt.plot([0 for _ in range(500)] + 
+                    np.convolve(total_rewards, np.ones((500,))/500, mode="valid").tolist())
+            plt.show()
